3d98fe20913949d9f023c3f258d104c7502afc49
Robert Szabo <robert.szabo@medius.se>
2017-08-23 15:21:09 +0200

    Test

---
+++ R/Cooc.R
@@ -0,0 +1,12 @@
+#' testCooc
+#'
+#' This function runs a test.
+#' @param data a character string.
+#' @keywords cooc
+#' @export
+#' @examples
+#' testCooc()
+
+testCooc <- function(data = "") {
+  return(data)
+}
