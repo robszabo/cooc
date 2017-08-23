6516e8bcbaf5d7d961589dcd8bca62a42f7563b6
Robert Szabo <robert.szabo@medius.se>
2017-08-23 16:43:49 +0200

    Updating data and added data generation

---
+++ R/GenData.R
@@ -0,0 +1,7 @@
+#d <- c("apa", "120ggg ost\\sron", "[katt]", "a342\\d{10} buffel")
+#dict <- matrix(c(d), nrow = 2, byrow = T)
+
+#save(dict, file = "data/dict.RData")
+#load("data/dict.RData")
+
+#dict <- edit(dict)
