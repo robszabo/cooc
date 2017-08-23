54e63606a0804482d0543321b3d405c88e9dc303
Robert Szabo <robert.szabo@medius.se>
2017-08-23 16:22:57 +0200

    Adding dataset

---
+++ R/dict-data.R
@@ -0,0 +1,17 @@
+#' Dict data
+#'
+#' Data from a dict.
+#'
+#' @docType data
+#'
+#' @usage data(dict)
+#'
+#' @format A dict dataset.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(dict)
+#' d <- length(dict)
+#'
+"dict"
